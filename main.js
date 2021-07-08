5d37f5131061f10b3349b672bfba879d68fd028c
CristianMauricioPicoRivero <cristian.pico@outlook.com>
2021-07-07 20:59:33 -0500

    Repaso de las variables y tipo de datos 07072021

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+nombreCompletoDelProfesor = `Cristian Mauricio Pico Rivero`;
+miEdad = 29;
+universidad = `Cooperativa de Colombia`
+altura = 1.83;
+Ciudad = `Bucaramanga`
+soyUnProfesor = false;
+Deporte = `Fútbol`
+Comida_favorita = `Ayacos de Mazorca`
+
+
+console.log(nombreCompletoDelProfesor, typeof(nombreCompletoDelProfesor));
+console.log(miEdad, typeof(miEdad));
+console.log(altura, typeof(altura));
+console.log(soyUnProfesor, typeof(soyUnProfesor));
+console.log(universidad, typeof (universidad));
+console.log(Deporte, typeof(Deporte));
+console.log(Comida_favorita, typeof(Comida_favorita));
+
+// alert()
+// document.write();
